098ecb8c3854e2d1b18f5e2708cc69b0f8f66937
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-06 13:48:12 +0000

    comment syntax rectified

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 def fizzbuzz():
     for i in range(1, 101):
         res = ""
-        if (i % 3 == 0):  #test multiples of 3
+        if (i % 3 == 0):  # test multiples of 3
             res += "Fizz"
-        if (i % 5 == 0):  #test multiples of 5
+        if (i % 5 == 0):  # test multiples of 5
             res += "Buzz"
         print("{}".format(res) or "{}".format(i), end='')
         print(" ", end='')
