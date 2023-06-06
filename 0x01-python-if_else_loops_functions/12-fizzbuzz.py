cd01fe3b44cbc61a415bc051cbf3c67fb4a590fb
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-06 13:46:35 +0000

    wrote fizzbuzz in py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        res = ""
+        if (i % 3 == 0):  #test multiples of 3
+            res += "Fizz"
+        if (i % 5 == 0):  #test multiples of 5
+            res += "Buzz"
+        print("{}".format(res) or "{}".format(i), end='')
+        print(" ", end='')
