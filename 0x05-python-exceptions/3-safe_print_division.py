9fdd69d9ab8ce994c413f2812e9badb21b241cf0
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-26 14:22:48 +0000

    handled ZeroDivision Error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,12 +5,10 @@ def safe_print_division(a, b):
     '''
     Divides two integers
     '''
-    inside_result = None
     try:
-        inside_result = (a/b)
-        return inside_result
-    except:
-        return None
+        result = a/b
+    except ZeroDivisionError:
+        result = None
     finally:
-        quotient = inside_result
-        print('{}'.format(quotient))
+        print("Inside result: {}".format(result))
+        return(result)
