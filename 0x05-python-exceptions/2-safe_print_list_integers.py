1a64e9112a2ee96d5dfdebee2167afbb4fa49b42
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-26 15:06:19 +0000

    Handled IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,18 +2,13 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    Prints the first x integers in a list and returns the number of integers
-    printed.
-    """
-    count = 0  # variable to keep track of the number of integers printed
-    if (my_list):
-
-        for i in range(x):  # Loop through the range from 0 to x-1
+    printed = 0
+    if(my_list):
+        for i in range(x):
             try:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1  # Increment the count of integers printed
-            except (IndexError, ValueError, TypeError):  # If an error occurs
-                break  # Exit the loop
-    print()  # Print a newline character after all the integers are printed
-    return count  # Return the count of integers printed
+                print("{:d}".format(my_list[i]), end='')
+                printed += 1
+            except(ValueError, IndexError, TypeError):
+                pass
+    print()
+    return(printed)
