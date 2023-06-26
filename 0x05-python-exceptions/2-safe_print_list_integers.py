af2cbb74702634ea846570969c7328ba169609b3
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-26 14:08:47 +0000

    mhandled TypeError and ValueError in the code.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,12 +7,13 @@ def safe_print_list_integers(my_list=[], x=0):
     printed.
     """
     count = 0  # variable to keep track of the number of integers printed
-    for i in range(x):  # Loop through the range from 0 to x-1
-        try:
-            if isinstance(my_list[i], int):  # Chk if elemnt at idx i is an int
+    if (my_list):
+
+        for i in range(x):  # Loop through the range from 0 to x-1
+            try:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1  # Increment the count of integers printed
-        except IndexError:  # If an IndexError occurs
-            break  # Exit the loop
+            except (IndexError, ValueError, TypeError):  # If an error occurs
+                break  # Exit the loop
     print()  # Print a newline character after all the integers are printed
     return count  # Return the count of integers printed
