07a653f87ed6ba21ee0a1a3f4faecb09715d0d3e
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-28 03:23:07 +0000

    necesary documentation added)

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+'''
+This module defines the Square class.
+'''
+
 class Square:
+    """
+    Represents a square class object
+
+    Attributes:
+    None.
+
+    Methods:
+    None
+    """
     pass
-    """
-    creates a square class object
-    """
