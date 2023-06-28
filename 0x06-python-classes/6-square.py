21a667f4338755ed00ffd5fb0a8a428aa445bc5e
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-28 10:58:39 +0000

    fixed too many blank lines error with pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,6 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
-
     def area(self):
         '''
         Calculates and returns area of a square based on size
