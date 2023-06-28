1c649e8724483044f81837171f7529964f14689a
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-28 11:11:53 +0000

    fixed too many blank lines error with pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
-'''This module defines a class Square that defines a square based on the script
+"""
+This module defines a class Square that defines a square based on the script
 5-square.py
-'''
+"""
 
 
 class Square:
-    '''
+    """
     Represents a square object.
-    '''
+    """
     def __init__(self, size=0, position=(0, 0)):
-        '''
+        """
         Initializes instance of a square
         Args:
         __size(int): size of the square
         Raises:
         TypeError: if size is not an integer.
         ValueError: if size is less than 0.
-        '''
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        '''
+        """
         Retrieves the size of the square.
         Returns:
         int: The size of the square.
-        '''
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        '''
+        """
         Sets the size of the square.
         Args:
         value (int): The new size of the square.
         Raises:
         TypeError: If value is not an integer.
         ValueError: If value is less than 0.
-        '''
+        """
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
@@ -47,12 +48,25 @@ class Square:
 
     @property
     def position(self):
+        """
+        Gets the position of the squre
+        Returns:
+        poisition (tuple)
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        '''sets the position of the square in 2d space
-        '''
+        """
+        sets the position of the square in 2d space.
+
+        Args:
+        value (tuple): The new position of the square
+
+        Raises:
+        TypeError: If value is not a tuple or contains non-integer values.
+        ValueError: If value contains non-positive integers.
+        """
         if not isinstance(value, tuple) or len(value) != 2 \
            or not all(isinstance(num, int) for num in value) or \
            not all(num >= 0 for num in value):
@@ -60,21 +74,21 @@ class Square:
         self.__position = value
 
     def area(self):
-        '''
+        """
         Calculates and returns area of a square based on size
         Returns:
         int: The area of the square
-        '''
+        """
         return(self.__size**2)
 
     def my_print(self):
-        '''
+        """
         Prints to stdout the square with the character #
         Args:
         self
         Attributes:
         None
-        '''
+        """
         if self.size == 0:
             print()
         for i in range(self.size):
