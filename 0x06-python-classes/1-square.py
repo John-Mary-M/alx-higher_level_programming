da0662271f6d83e5d13db7864eab13e19eee59a7
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-28 03:44:10 +0000

    ALL documentation added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+'''
+This module defines a sqaure class based on 0-sqaure.py script in this directory
+'''
+
 class Square:
     """
     creates a square object
     """
     def __init__(self, size=0):
-        if(type(size) is not int):
-            raise TypeError("size must be an integer")
-        if(size < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = size
         """
         initializes instance of a square
         Args:
             __size(int): size of square
             size must be positive and integer type
         """
+        if(type(size) is not int):
+            raise TypeError("size must be an integer")
+        if(size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
