e9a712bcce2f571d1ad10b27bc8df92f576eec5b
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-28 10:08:07 +0000

    fixed doc string issue

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a sqaure class based on 0-sqaure.py script in this
+"""
+This module defines a sqaure class based on 0-sqaure.py script in this
 directory
 """
 
