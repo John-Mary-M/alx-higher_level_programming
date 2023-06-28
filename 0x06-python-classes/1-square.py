c1c02438e017f53907741e28b5d7f18ac8af8270
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-28 06:02:01 +0000

    fixed pycodestyle bug

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 '''
-This module defines a sqaure class based on 0-sqaure.py script in this directory
+This module defines a sqaure class based on 0-sqaure.py script in this
+directory
 '''
+
 
 class Square:
     """
