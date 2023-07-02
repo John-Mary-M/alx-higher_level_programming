3dfebc7e35880bca425fd31deac04ee44b5383ba
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-02 10:25:28 +0000

    included more tests for max_integer()

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -24,5 +24,21 @@ class TestMaxInteger(unittest.TestCase):
         result = max_integer([-1, 0, 5, -10, 3])
         self.assertEqual(result, 5)
 
+    def test_duplicate_numbers(self):
+        result = max_integer([3, 2, 3, 1, 3, 4])
+        self.assertEqual(result, 4)
+
+    def test_single_element_list(self):
+        result = max_integer([5])
+        self.assertEqual(result, 5)
+
+    def test_empty_list(self):
+        with self.assertRaises(TypeError):
+            result = max_integer([])
+
+    def test_large_list(self):
+        result = max_integer(list(range(1, 1000001)))
+        self.assertEqual(result, 1000000)
+
 if __name__ == '__main__':
     unittest.main()
