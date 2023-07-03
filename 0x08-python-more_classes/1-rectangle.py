d3eeadffc77a4447e561f79852dbf2100ed5bee2
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-03 13:42:24 +0000

    wrote 1-rectangle.py gave it private attributes of height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module is a definition of a Rectangle class"""
+
+
+class Rectangle:
+    """Makes rectangles"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height of rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
