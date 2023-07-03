77f7b6c017e4a7e1c5efcb34a36c96975172f770
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-03 18:12:00 +0000

    removed () in __repr__ method's return

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle():
         """
         if not self.perimeter():
             return ""
-        return('\n'.join('#' * self.width for x in range(self.height)))
+        return '\n'.join('#' * self.width for x in range(self.height))
 
 
     def __repr__(self):
