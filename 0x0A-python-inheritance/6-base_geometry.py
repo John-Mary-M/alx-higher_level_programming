2bf0af370e57240c89fffd967f9c144eb54df2f0
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-10 13:43:55 +0000

    wrote 6-base_geometry.py Public instance method: def area(self): that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """
+    Empty class
+    """
+    def __init__(self):
+        """initialzes empty class"""
+        pass
+
+    def area(self):
+        """
+        Raises Exception
+        """
+        raise Exception("area() is not implemented")
