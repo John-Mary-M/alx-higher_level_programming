c6fceb5c4b45d81ac6aafb5f2e1fb50891d8fb75
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-11 02:18:27 +0000

    added more in the function docstring

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,7 +3,13 @@
 
 
 def add_attribute(obj, attr, value):
-    """adds new attribute"""
+    """adds new attribute
+
+    Args:
+    obj (__obj__): object to which to add attributes
+    attr (__str/int/bool__): attribute to add
+    value (__str__): value of attribute.
+    """
     if('__slots__' in dir(obj) or '__dict__' not in dir(obj) or
        hasattr(obj, attr)):
         raise TypeError("can't add new attribute")
